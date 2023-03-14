2f8dad05a7529ce2b21f16408b0a060002c2c271
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-03-14 21:05:03 +0000

    Finished Getting Older Walkthrough

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,4 +9,19 @@ describe('constructor', () => {
 
         expect(pet.name).toEqual('Fido');
     });
+    test('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    test('incrememnts the age by 1', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
 });
