f777095ca1d0f0adbb1f23b15cc20e3623b86c18
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-03-14 20:44:49 +0000

    Completed Naming Pets Walkthrough

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,12 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
+    test('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+    test('sets the name property', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.name).toEqual('Fido');
     });
 });
